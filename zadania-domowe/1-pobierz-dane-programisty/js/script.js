b5cccd73b2b927160a296ed2b5454e1dfcaba27b
Unknown <marrcel12.marga@gmail.com>
2018-01-18 16:13:36 +0100

    after

--- zadania-domowe/1-pobierz-dane-programisty/js/script.js
+++ zadania-domowe/1-pobierz-dane-programisty/js/script.js
@@ -2,11 +2,11 @@
 
      $('#button').click(function () {
          $.getJSON('https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php', function (data) {
-             $(data).each(function (element, index) {
-
-                 $('#button').append("<div id=id=dane-programisty>" + this.imie + " " + this.nazwisko + " " + this.zawod + " " + this.firma + "</div>");
+            $(data).each(function(element,index){
+                
+                 $('#button').after("<div id=id=dane-programisty>"+this.imie+" "+this.nazwisko+ " "+ this.zawod+" "+this.firma+"</div>");
              });
-
+             
          });
-     });
+     });    
  });
