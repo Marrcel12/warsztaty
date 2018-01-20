8cf20ff08828b8a8ff0561cf640402d8df23baf7
Unknown <marrcel12.marga@gmail.com>
2018-01-20 09:05:41 +0100

    dodanie var

--- zadania-domowe/Suma-wyplat/js/script.js
+++ zadania-domowe/Suma-wyplat/js/script.js
@@ -1,6 +1,6 @@
  $(function () {
      $('#count-sum').click(function () {
-         sum = 0;
+          var sum = 0;
          $(".salary").each(function () {
              sum = sum + parseInt($(this).text());
              $("#sum").text(sum);
