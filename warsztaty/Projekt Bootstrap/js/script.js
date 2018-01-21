060c8a274616ef42b2fbc178d44066a79d310ba0
Unknown <marrcel12.marga@gmail.com>
2018-01-21 14:57:22 +0100

    Wypychanie

---
+++ warsztaty/Projekt Bootstrap/js/script.js
@@ -0,0 +1,12 @@
+$(function(){
+$(window).scroll(function(){
+if($(window).scrollTop() >= 50) {
+    $("#main-nav").addClass("scroll");
+    $("#main-nav").css({"transition": "all 0.3s"});
+    
+}
+        else{
+            $("#main-nav").removeClass("scroll");
+        }
+    });
+});
