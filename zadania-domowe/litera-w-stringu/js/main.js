76daeb9bc0a952930e07bdc05539e7b58281e9dd
Unknown <marrcel12.marga@gmail.com>
2018-02-28 10:05:25 +0100

    Auto stash before merge of "master" and "origin/master"

--- zadania-domowe/litera-w-stringu/js/main.js
+++ zadania-domowe/litera-w-stringu/js/main.js
@@ -1,11 +1,16 @@
  function literWStringu(a, j) {
      var licznik=0;
      for (var i = 0; i < a.length; i++) {
+<<<<<<< Updated upstream
          if(a.charAt(i)==j||j.toUpperCase()==a.charAt(i)){
+=======
+         if(a.charAt(i)==j||j.toUpperCase==a.charAt(i)){
+>>>>>>> Stashed changes
              licznik++
          }
      }
     return licznik;
  }
 
-console.log(literWStringu("Szkoła Programowania Akademia 108","a"))
+
+alert(literWStringu(prompt("Podaj napis"),prompt("Podaj literę")));
